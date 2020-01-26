95ad89e46134ee903e96bcf74a0dbcc2eb957a69
edsonbassani <edsonbassani@gmail.com>
2020-01-26 15:24:14 -0800

    Initial commit

---
+++ Altran.WeirdNumbers/WeirdNumbers.Businness/Validation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WeirdNumbers.Businness
+{
+    public static class Validation
+    {
+        
+        public static string EvalNumber(string param)
+        {
+            bool res;
+            res = int.TryParse(param, out _);
+
+            if (!res)
+                return "Booooom!!!!";
+            else
+               return GetDivisors(int.Parse(param));
+        }
+
+        private static string GetDivisors(int number)
+        {
+            int contador = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    contador += i;
+
+                    if (contador > number)
+                    {
+                        string output = String.Format("Weird Number: {0}", contador);
+                        return output;
+                    }
+                }
+            }
+
+            if (contador <= number)
+                return "Not an Weird Number";
+
+            return "";
+        }
+    }
+}
---
+++ Altran.WeirdNumbers/WeirdNumbers.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WeirdNumbers.Businness;
+
+namespace WeirdNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            CheckInput();
+        }
+
+        static void CheckInput()
+        {
+            Console.WriteLine("Input Number:");
+            var input = Console.ReadLine();
+            var output = Validation.EvalNumber(input);
+            Console.WriteLine(output);
+        }
+
+    }
+}
